be5d2429b7b4c7e284067a60b9bc3221c4e3a3db
vicwlau <vicwlau@gmail.com>
2024-08-31 00:02:36 -0700

    typewriter effect and matrix numbers

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
 
-const intro = `Hi, I am Victor and this is my app development journal!`;
-document.querySelector('.introduction').textContent = intro;
+const intro_1 = `development journal`;
+const intro_2 = 'by: victor lau';
+
+document.querySelector('.introduction-1').textContent = intro_1;
+document.querySelector('.introduction-name').textContent = intro_2;
 
 
-// function drawVectorLineAcrossScreen(){
-//     const canvas = document.querySelector('canvas');
-//     const ctx = canvas.getContext('2d');
-    
-//     //get screen width
-//     const screenWidth = window.innerWidth;
-//     console.log(screenWidth);
-    
-//     ctx.beginPath();
-//     ctx.moveTo(0, 0);
-//     ctx.lineTo(screenWidth, 0);
-//     //make line thicker
-//     ctx.lineWidth = 15;
-    
-//     //
-//     const lineColorStart = 'rgba(0, 0, 0, 1)';
-//     const lineColorEnd = 'rgba(0, 0, 0, 0)';
-//     //make line fade out from black to transparent left to right
-//     const gradient = ctx.createLinearGradient(0, 0, screenWidth, 0);
-//     gradient.addColorStop(0, lineColorStart);
-//     gradient.addColorStop(1, lineColorEnd);
-//     ctx.strokeStyle = gradient;
-//     ctx.stroke();
-// }
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
 
-// function createTiles(){
-//     // create a new div element with class tile
-//     const tile = document.createElement('tile');
-//     tile.classList.add('tile');
-//     document.querySelector('.tile-container').appendChild(tile);
-// }
+//making the canvas full screen
+c.height = 800;
+c.width = 1200;
 
-// function randomizeTileDimensions(){
-//     const tiles = document.querySelectorAll('.tile');
-//     tiles.forEach(tile => {
-//         const width = Math.floor(100+ Math.random() * 100);
-//         const height = Math.floor(100 + Math.random() * 200);
-//         tile.style.width = `${width}px`;
-//         tile.style.height = `${height}px`;
-//     });
-// }
+//chinese characters - taken from the unicode charset
+var words = ["0","1"];
+//converting the string into an array of single characters
 
-// for (let i = 0; i < 25; i++){
-//     createTiles();
-// }
 
-//randomizeTileDimensions();
-//drawVectorLineAcrossScreen();
+var font_size = 35;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+	drops[x] = 1; 
 
+//drawing the characters
+function draw()
+{
+   var grd = ctx.createLinearGradient(0, 0, 200, 0);
+    grd.addColorStop(0, "white");
+    grd.addColorStop(1, "rgba(100,100,100,0.6)");
+	
+    //Black BG for the canvas
+	//translucent BG to show trail
+	ctx.fillStyle = "rgba(255, 255, 255, .15)";
+	ctx.fillRect(0, 0, c.width, c.height);
+	
+	ctx.fillStyle = grd; //green text
+    //use Noto Sans SC for chinese characters
+	ctx.font = font_size + "px Noto Sans SC";
+	//looping over drops
+	for(var i = 0; i < drops.length; i++)
+	{
+		//a random chinese character to print
+		var text = words[Math.floor(Math.random()*words.length)];
+		//x = i*font_size, y = value of drops[i]*font_size
+		ctx.fillText(text, i*font_size, drops[i]*font_size);
+		
+		//sending the drop back to the top randomly after it has crossed the screen
+		//adding a randomness to the reset to make the drops scattered on the Y axis
+		if(drops[i]*font_size > c.height && Math.random() > 0.975)
+			drops[i] = 0;
+		
+		//incrementing Y coordinate
+		drops[i] += 1;
+	}
+}
+
+setInterval(() => {
+    setTimeout(draw, 1700);
+}, 33);
