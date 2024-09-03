bc689c79956ef5ad1eb6c55101368a79aeefce47
vicwlau <vicwlau@gmail.com>
2024-09-03 00:08:43 -0700

    added twojs library and background image

--- Examples/matrix_demo.js
+++ Examples/matrix_demo.js
@@ -1,9 +1,4 @@
 
-const intro_1 = `development journal`;
-const intro_2 = 'by: victor lau';
-
-document.querySelector('.introduction-1').textContent = intro_1;
-document.querySelector('.introduction-name').textContent = intro_2;
 
 
 var c = document.getElementById("c");
---
+++ intro.js
@@ -0,0 +1,5 @@
+const intro_1 = `development journal`;
+const intro_2 = 'by: victor lau';
+
+document.querySelector('.introduction-1').textContent = intro_1;
+document.querySelector('.introduction-name').textContent = intro_2;
