ec0236a7e7d6ab9b07dd04dc042e1f144567c708
vicwlau <vicwlau@gmail.com>
2024-09-04 10:42:39 -0700

    separated head_bg js logic

---
+++ header_bg.js
@@ -0,0 +1,31 @@
+// import {two} from 'two.js';
+
+addBackdrop(25);
+
+function addBackdrop(d) {
+    
+    const dimensions = d || 50;
+    const two = new Two({
+    type: Two.Types.canvas,
+    width: dimensions,
+    height: dimensions
+    });
+
+    const r = dimensions / 5;
+    const center = dimensions / 2;
+
+    const a = two.makeLine(center - r, center, center + r, center);
+    const b = two.makeLine(center, center - r, center, center + r);
+
+    a.stroke = b.stroke = '#aaa';
+    a.linewidth = b.linewidth = 0.25;
+
+    two.update();
+
+    const container = document.querySelector("header");
+    // container.style.width = "100%";
+    // container.style.height = "600px";
+    container.style.backgroundImage = `url(${two.renderer.domElement.toDataURL()})`;
+    container.style.backgroundRepeat = 'repeat';
+    container.style.backgroundSize = `${dimensions}px`;
+}
